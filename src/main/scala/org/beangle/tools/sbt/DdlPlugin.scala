c16d2c51b41745db1c0d695cef29a6e0eff33557
段体华 <duantihua@gmail.com>
2021-10-31 21:02:49 +0800

    rebase on parent 0.4.0

--- src/main/scala/org/beangle/tools/sbt/DdlPlugin.scala
+++ src/main/scala/org/beangle/tools/sbt/DdlPlugin.scala
@@ -75,7 +75,7 @@ object DdlPlugin extends sbt.AutoPlugin {
     }
 
   def report(m2Root: String, xmlFile: File, target: File, log: util.Logger): Unit = {
-    val rs = Dependency.resolveJar(m2Root, "org.beangle.db:beangle-db-report_3:0.0.13")
+    val rs = Dependency.resolveJar(m2Root, "org.beangle.db:beangle-db-report_3:0.0.14")
     if (rs._1) {
       val reportDir = new File(target.getAbsolutePath + "/dbreport/")
       reportDir.mkdirs()
