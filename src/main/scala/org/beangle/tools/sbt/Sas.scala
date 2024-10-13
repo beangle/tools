63e5347ec2c7cd6063a722289cc205ab47a6331f
段体华 <duantihua@gmail.com>
2024-10-13 21:12:11 +0800

    update to sas 0.12.7

--- src/main/scala/org/beangle/tools/sbt/Sas.scala
+++ src/main/scala/org/beangle/tools/sbt/Sas.scala
@@ -20,7 +20,7 @@ package org.beangle.tools.sbt
 import sbt.*
 
 object Sas {
-  val Engine = "org.beangle.sas" % "beangle-sas-engine" % "0.12.6" % "test"
+  val Engine = "org.beangle.sas" % "beangle-sas-engine" % "0.12.7" % "test"
   val Tomcat = "org.apache.tomcat.embed" % "tomcat-embed-core" % "10.1.30" % "test" exclude("org.apache.tomcat", "tomcat-annotations-api")
   val Undertow = "io.undertow" % "undertow-servlet" % "2.3.17.Final" % "test"
 }
